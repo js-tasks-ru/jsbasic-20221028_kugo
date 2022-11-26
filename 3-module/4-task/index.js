3d0cfceca434d98f2c60d35c7609ffa5d00357a6
NoNaKugo <worktoffi@gmail.com>
2022-11-26 22:36:23 +0300

    3 modul 1-5 tasks

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,17 @@
+
 function showSalary(users, age) {
-  // ваш код...
+
+  //##вот мое решение##
+  // let resh = [];
+  // users.map((item) => {
+  //   if (item => item.age <= age) {
+  //     resh.push(`${item.name} , ${item.balance}`);
+  //   }
+  // });
+  // return resh.join(`\n`);
+  
+  return data
+  .filter(item => item.age <= age)
+  .map(item => `${item.name}, ${item.balance}`)
+  .join('\n');
 }
