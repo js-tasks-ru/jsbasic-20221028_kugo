ed326b7d5213cfd1dc4bcd47b900e4db941f5517
NoNaKugo <worktoffi@gmail.com>
2022-12-11 14:53:31 +0300

    resolt

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,22 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+
+    this.render();
   }
+
+
+  render() { 
+    this.elem = createElement(`
+                              <div class="ribbon">
+                                <button class="ribbon__arrow ribbon__arrow_left">
+                                  <img src="/assets/images/icons/angle-icon.svg" alt="icon" />
+                                </button>
+                                <nav class="ribbon__inner"></nav>
+                                <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+                                  <img src="/assets/images/icons/angle-icon.svg" alt="icon" />
+                                </button>
+                              </div>`);
+  }
+  
 }
